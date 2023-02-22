92e91a511ba1edd5c5c9cff02ccef9cb16f52f79
SergiikoVasya <sergiikovasya@gmail.com>
2023-02-22 17:26:33 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+const userName = prompt('Введіть своє ім\'я')
+const userLastName = prompt('Введіть своє прізвище')
+const userAge = prompt('Введіть свій вік')
+
+console.log(`Мене звуть ${userName} ${userLastName}. Мені ${userAge} років!!!:)`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
